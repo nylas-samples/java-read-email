116942845ff5f5ace37f9753cd4376b50ec21e2e
Blag <atejada@gmail.com>
2023-12-14 16:20:36 -0500

    Update ReadInbox.java

--- src/main/java/ReadInbox.java
+++ src/main/java/ReadInbox.java
@@ -1,37 +1,30 @@
-//Import Java Utilities
-import java.io.IOException;
-import java.util.List;
-
-// Import Nylas Packages
-import com.nylas.NylasAccount;
+// Import Nylas packages
 import com.nylas.NylasClient;
-import com.nylas.RequestFailedException;
-import com.nylas.Message;
-import com.nylas.Messages;
-import com.nylas.RemoteCollection;
-import com.nylas.MessageQuery;
+import com.nylas.models.*;
 
 // Import DotEnv to handle .env files
 import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvException;
 
-public class ReadInbox {
-    public static void main(String[] args) throws RequestFailedException, IOException{
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+public class ReadEmailParameters {
+    public static void main(String[] args) throws NylasSdkTimeoutError, NylasApiError {
+        // Load the .env file
         Dotenv dotenv = Dotenv.load();
-        // Create the client object
-        NylasClient client = new NylasClient();
-        // Connect it to Nylas using the Access Token from the .env file
-        NylasAccount account = client.account(dotenv.get("ACCESS_TOKEN"));
+        // Initialize the Nylas client
+        NylasClient nylas = new NylasClient.Builder(dotenv.get("V3_TOKEN_API")).build();
 
-        // Access the messages endpoint
-        Messages messages = account.messages();
-        // Read 3 messages coming from the Inbox
-        RemoteCollection<Message> messageList = messages.list(new MessageQuery().in("inbox").limit(3));
+        ListMessagesQueryParams queryParams = new ListMessagesQueryParams.Builder().
+                limit(3).
+                inFolder(List.of("inbox")).
+                build();
 
-        // Loop through the emails
-        for(Message email : messageList){
-            // Print the date and subject of the email
-            System.out.println(email.getDate() + " " + email.getSubject());
+        ListResponse<Message> message = nylas.messages().list(dotenv.get("GRANT_ID"), queryParams);
+        for(Message email : message.getData()){
+            String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").
+                    format(new java.util.Date((email.getDate() * 1000L)));
+            System.out.println("[" + date + "] | " + email.getSubject());
         }
     }
 }
